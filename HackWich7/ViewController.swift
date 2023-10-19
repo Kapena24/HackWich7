9e5a17b3183036125134c9a807f895d58eebfb25
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-10-18 19:41:18 -1000

    Completed and recorded

--- HackWich7/ViewController.swift
+++ HackWich7/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayLabel.text = ""
     }
-
-
+   
+    @IBAction func setLabelButtonPressed(_ sender: UIButton) {
+        if let userInputText = textField.text {
+            displayLabel.text = userInputText
+        }
+    }
+    
 }
 
